80aa8fc5eae7c6811e117d74eebf31c9428f423a
Felipe Hermosilla <felipe.hermosilla@edge.com.py>
2019-01-25 20:29:31 -0300

    Understanding the 'state' attribute

--- src/App.js
+++ src/App.js
@@ -3,14 +3,22 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    person:[
+      {name:'Max', age:30},
+      {name:'Manu', age:33},
+      {name:'Rex', age:32}
+    ]
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm a React app</h1>
         <p>This is really working!</p>
-        <Person name="Max" age="30"></Person>
-        <Person name="Manu" age="33"></Person>
-        <Person name="Rex" age="32">My Hobbie is: soccer</Person>
+        <Person name={this.state.person[0].name} age={this.state.person[0].age}></Person>
+        <Person name={this.state.person[1].name} age={this.state.person[1].age}></Person>
+        <Person name={this.state.person[2].name} age={this.state.person[2].age}>My Hobbie is: soccer</Person>
       </div>
     );
     //return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does it work now!'));
