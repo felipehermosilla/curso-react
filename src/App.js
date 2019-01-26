8742cb5fa35b0ece787a4ec5207f06c983758b0e
Felipe Hermosilla <felipe.hermosilla@edge.com.py>
2019-01-25 22:40:12 -0300

    Creating the first event with method

--- src/App.js
+++ src/App.js
@@ -10,12 +10,15 @@ class App extends Component {
       {name:'Rex', age:32}
     ]
   }
-
+  switchNameHandler = () =>{
+   console.log('clicked!!');
+  }
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm a React app</h1>
         <p>This is really working!</p>
+        <button onClick={this.switchNameHandler}>Click on me!</button>
         <Person name={this.state.person[0].name} age={this.state.person[0].age}></Person>
         <Person name={this.state.person[1].name} age={this.state.person[1].age}></Person>
         <Person name={this.state.person[2].name} age={this.state.person[2].age}>My Hobbie is: soccer</Person>
