5d9e7edad6b8f57767fc0da66bec68f3857126e6
Felipe Hermosilla <felipe.hermosilla@edge.com.py>
2019-12-07 12:41:38 -0300

    Setting classes dynamically

--- src/App.js
+++ src/App.js
@@ -79,10 +79,17 @@ class App extends Component {
       );
       style.backgroundColor = 'red';
     }
+    const classes = [];
+    if(this.state.persons.length<=2){
+      classes.push('red');
+    }
+    if(this.state.persons.length<=1){
+      classes.push('bold');
+    }
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
-        <p>This is really working!</p>
+        <p className={classes.join(' ')}>This is really working!</p>
         <button style={style} onClick={this.tooglePersonHandler}>Switch Name</button>
         {persons}
       </div>
