74c21335a5da1c53deedfb5745f2f48c724957ff
Felipe Hermosilla <felipe.hermosilla@edge.com.py>
2020-04-18 11:28:09 -0400

    More on Syled Component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,21 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
 import './App.css';
 import Person from './Person/Person';
+
+const StyledButton = styled.button`
+  background-color: green;
+  color: white;
+  font: inherit;
+  border: 1px solid blue;
+  padding: 8px;
+  cursor: pointer;
+
+  &:hover {
+    background-color: lightgreen;
+    color: black;
+  }
+`;
 
 class App extends Component {
   state = {
@@ -98,7 +113,9 @@ class App extends Component {
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p className={classes.join(' ')}>This is really working!</p>
-        <button style={style} onClick={this.tooglePersonHandler}>Switch Name</button>
+        <StyledButton onClick={this.tooglePersonHandler}>
+          Toogle Person
+        </StyledButton>
         {persons}
       </div>
     );
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -17,11 +17,6 @@ const StyledDiv = styled.div`
 `;
 
 const person = ( props ) => {
-    const style = {
-      '@media (min-width: 500px)': {
-        width: '450px'
-      }
-    };
     return (
         <StyledDiv>
             <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p>
