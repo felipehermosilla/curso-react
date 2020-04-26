b1733fd160b70b352063337c2f0e6a3141d470f3
Felipe Hermosilla <felipe.hermosilla@edge.com.py>
2020-04-25 20:43:56 -0400

    Splitting the app into components

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
+
 import classes from './App.module.css';
-import Person from '../components/Persons/Person/Person';
+import Persons from '../components/Persons/Persons';
+import Cockpit from '../components/Cockpit/Cockpit';
 
 class App extends Component {
   state = {
@@ -54,37 +56,17 @@ class App extends Component {
  }
   render() {
     let persons = null;
-    let btnClass = '';
     if(this.state.showPersons){
-      persons = (
-        <div>
-          {this.state.persons.map((person, index) => {
-            return <Person
-              name={person.name}
-              age={person.age}
-              click={() => this.deletePersonHandler(index)}
-              key={person.id}
-              changed={(event)=> this.switchNameHandler(event, person.id)}
-            />
-          })}
-        </div>
-      );
-      btnClass = classes.Red;
-    }
-    const assignedClasses = [];
-    if(this.state.persons.length<=2){
-      assignedClasses.push(classes.red);
-    }
-    if(this.state.persons.length<=1){
-      assignedClasses.push(classes.bold);
+      persons = <Persons
+          persons={this.state.persons}
+          clicked={this.deletePersonHandler}
+          changed={this.switchNameHandler} />;
     }
     return (
       <div className={classes.App}>
-        <h1>Hi, I'm a React App</h1>
-        <p className={assignedClasses.join(' ')}>This is really working!</p>
-        <button className={btnClass} onClick={this.tooglePersonHandler}>
-          Toogle Person
-        </button>
+        <Cockpit showPersons={this.state.showPersons}
+        persons={this.state.persons}
+        clicked={this.tooglePersonHandler} />
         {persons}
       </div>
     );
